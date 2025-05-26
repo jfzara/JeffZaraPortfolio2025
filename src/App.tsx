65f60ea4c820e530bed929f46bdc397caca7bb44
jfzara <zarajeanfabrice@gmail.com>
2025-05-26 13:34:00 -0400

    Refactorisation du code et ajout de commentaires

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+// Modifié le 2025-05-26 13:34 - Refactorisation du code et ajout de commentaires
 // Modifié le 2025-05-23 15:48 - Petits ajustements dans la structure
 // Modifié le 2025-05-23 15:19 - Test du layout avec Header et Footer
 // Modifié le 2025-05-23 09:37 - Ajout des fichiers main.tsx et App.tsx
--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+// Modifié le 2025-05-26 13:34 - Refactorisation du code et ajout de commentaires
 // Modifié le 2025-05-23 09:37 - Ajout des fichiers main.tsx et App.tsx
 // Modifié le 2025-05-25 13:34 - Refactorisation du code et ajout de commentaires
 // Modifié le 2025-05-22 09:37 - Ajout des fichiers main.tsx et App.tsx
